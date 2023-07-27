838c8aac59febc7e6d1c52b569ec72d1da6532c8
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-07-27 12:28:20 -0300

    back-end CORS configured

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -5,8 +5,30 @@ require('dotenv').config();
 
 const app = express();
 
+let whiteList;
+
+try {
+  whiteList = JSON.parse(process.env.WHITE_LIST);
+} catch(error) {
+  whiteList = [];
+  // eslint-disable-next-line no-console
+  console.log(new Error(
+    'WHITE_LIST is not a valid json. Check his value and structure and restart the server.',
+  ));
+}
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback();
+    }
+  },
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(router);
 
 require('./database/connect');
